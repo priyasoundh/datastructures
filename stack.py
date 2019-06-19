dd3496d45953dcf0b2296c8beabcda79f420cd54
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-19 21:28:20 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,91 @@
+class Node:
+  def __init__(self,data):
+    self.data=data
+    self.next=None
+class Singly_linked:
+  def __init__(self):
+    self.head=None
+    self.tail=None
+
+  def insert_end(self,data):
+    if(self.head==None):
+      newnode=Node(data)
+      self.head=newnode
+      self.tail=newnode
+    else:
+      newnode=Node(data)
+      self.tail.next=newnode
+      self.tail=self.tail.next
+    newnode=None
+
+  def delete_end(self):
+    temp=self.head
+    while(temp.next.next!=None):
+      temp=temp.next
+    temp.next=None
+
+  def peak(self):
+    temp=self.head
+    count=1
+    while(temp.next!=None):
+      temp=temp.next
+      count=count+1
+    return count
+
+  def search(self,x):
+    temp=self.head
+    while(temp!=None):
+      if(temp.data==x):
+        return True
+      temp=temp.next
+    return False
+   
+  def le(self):
+    temp=self.head
+    count=0
+    while(temp!=None):
+      count=count+1
+      temp=temp.next
+    return count
+
+  def display(self):
+    temp=self.head
+    while(temp.next!=None):
+      print(temp.data,end='->')
+      temp=temp.next
+    print(temp.data)
+
+def menu():
+  print("Linkedlist")
+  s=Singly_linked()
+  while(1):
+    print("1-->insert")
+    print("2-->delete")
+    print("3-->search")
+    print("4-->display")
+    print("5-->count number of elements")
+    print("6-->peek")
+    print("7-->exit")
+    x=int(input())
+    if(x==1):
+      val=input("data to insert:")
+      s.insert_end(val)
+    elif(x==2):
+      s.delete_end()
+    elif(x==3):
+      a=input("data:")
+      b=s.search(a)
+      if(b):
+        print("yes")
+      else:
+        print("No")
+    elif(x==4):
+      s.display()
+    elif(x==5):
+      print("count:",s.le())
+    elif(x==6):
+      print("peak:",s.peak())
+    elif(x==7):
+      break
+
+menu()
